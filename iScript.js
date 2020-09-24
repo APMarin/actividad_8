32ba86dc80080b60163ee65bbacd9f2dd2eacbaa
Andres <a307737@uach.mx>
2020-09-24 17:02:23 -0600

    Bienvenida

---
+++ iScript.js
@@ -0,0 +1,9 @@
+const tipoGenero = () =>{
+    nam1 = 'Juanito';
+    nam2 = 'Juanita';
+    gender = document.getElementById('genero').value;
+
+    alert(gender == 0 ? `Bienvenido ${nam1}` : `Bienvenida ${nam2}`);
+    
+
+}
